37d8ed09288ea96e4dd21a6cf493d7d2207274ee
fexra <fexra@protonmail.com>
2018-07-10 14:50:51 +0200

    mod rewrite problem + no blockexplorer arg on - keep localhost

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-        'api' => 'http://athx.trtl.services:12001',
+        'api' => 'http://127.0.0.1:12001',
         'blockTargetInterval' => 3600,
         'coinUnits' => 100
 );
