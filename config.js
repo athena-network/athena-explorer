1ad962672ed7c81ecf326d67fefc002475de7323
fexra <fexra@protonmail.com>
2018-07-13 11:34:41 +0200

    added funkypenquin pool

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ var networkStat = {
  "athx": [
 	["pool.athx.host", "http://pool.athx.host:1117"],
 	["athena.pool.yt", "https://athena.pool.yt:3512"],
+	["athx.heigh-ho.funkypenguin.co.nz", "https://api.athx.heigh-ho.funkypenguin.co.nz"],
  ]
 };
 
